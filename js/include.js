e012e73ad71ec6cac7146ae19712e2afce2afb5a
VisitMalvan <haribolnishad@gmail.com>
2026-03-14 19:43:18 +0530

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,4 +1,14 @@
-async function loadIncludes() {
+async function loadIncludes(){
+
+const header = await fetch('/includes/header.html');
+document.getElementById("header").innerHTML = await header.text();
+
+const footer = await fetch('/includes/footer.html');
+document.getElementById("footer").innerHTML = await footer.text();
+
+}
+
+loadIncludes();async function loadIncludes() {
 
 const header = await fetch('/includes/header.html');
 const headerHTML = await header.text();
