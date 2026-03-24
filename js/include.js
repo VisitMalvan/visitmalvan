5f766b74709acfa37011102f6631e1d82777c504
VisitMalvan <haribolnishad@gmail.com>
2026-03-24 18:26:57 +0530

    Update include.js

--- js/include.js
+++ js/include.js
@@ -9,3 +9,12 @@ async function loadIncludes() {
 }
 
 loadIncludes();
+function toggleMenu() {
+  var menu = document.getElementById("mobileMenu");
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
